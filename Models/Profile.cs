1d93feb3c5a2f9733bacfe87656bab860b116c29
Aimen <djeaimen@gmail.com>
2023-11-04 21:32:04 -0400

    page home complétée

--- Models/Profile.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SuiviFitness.Models
-{
-    public class Profile
-    {
-        public string? description { get; set; }
-        public List<string>? preferenceAlimentaire { get; set; } // "Végétarien" ou "Carnivor" ou "Mixte"
-        public List<string>? niveauDeFitness { get; set; } // "Débutant" ou "Intermédiaire" ou "Avancé"
-    }
-}
