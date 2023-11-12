db0427378308aa5de0dc1e410a1336f177fffd6d
Aimen <djeaimen@gmail.com>
2023-11-12 15:57:29 -0500

    Ajout de la page contact

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,10 +23,11 @@ namespace SuiviFitness.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult contact()
         {
             return View();
         }
+        
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
