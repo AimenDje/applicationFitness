c2b350ffe35c215782e33b718c187a021b3948e5
Serge <smilongo@dept-info.crosemont.quebec>
2023-12-13 23:49:38 -0500

    Suppression des class ambigue du package Models

--- Models/HistoriqueActivitees.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SuiviFitness.Models
-{
-    public class HistoriqueActivitees
-    {
-        public List<ProgrammeEntrainement> liste_entraiments_effectuer { get; set; }
-        public List<MesureCorporelle> list_mesures_corporelle { get; set; }
-        public List<Objectif> list_des_objetifs { get; set; }
-    }
-}
