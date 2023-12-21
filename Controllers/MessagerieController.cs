968bee0a15860f5c1411285f1dc23af5cbef6dc8
Serge <smilongo@dept-info.crosemont.quebec>
2023-12-21 16:29:20 -0500

    Implémentation d'un chat avec SignalR

---
+++ Controllers/MessagerieController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SuiviFitness.Controllers
+{
+    public class MessagerieController : Controller
+    {
+        public IActionResult Messagerie()
+        {
+            return View();
+        }
+    }
+}
