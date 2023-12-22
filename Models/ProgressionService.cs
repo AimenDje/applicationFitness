fc17783ae3e09c28f0060f3f7fe59fad4128fe2c
Aimen <djeaimen@gmail.com>
2023-12-21 21:26:27 -0500

    ajout de boutton effectuer

---
+++ Models/ProgressionService.cs
@@ -0,0 +1,24 @@
+﻿namespace SuiviFitness.Models
+{
+    public class ProgressionService
+    {
+        private Utilisateur _utilisateur;
+
+        public ProgressionService()
+        {
+            // Initialisez ou obtenez l'instance de l'utilisateur selon vos besoins
+            _utilisateur = new Utilisateur();
+        }
+
+        public int GetProgression()
+        {
+            return _utilisateur.progression;
+        }
+
+        public void IncrementerProgression(int increment)
+        {
+            _utilisateur.progression += increment;
+            // Ici, vous pouvez également gérer la persistance si nécessaire, même si ce n'est pas dans une base de données
+        }
+    }
+}
